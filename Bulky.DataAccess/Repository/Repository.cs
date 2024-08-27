93500c2308cba258fbc4f6b6a5fdfc9951b76a9f
adel <adel@gmail.com>
2024-08-27 18:48:31 +0300

    someFeaturesInProductController

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -1,45 +1,65 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
         private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+
         public Repository(AppDbContext db)
         {
             _db = db;
+            this.dbSet = _db.Set<T>();
         }
+
         public void Add(T entity)
         {
-            _db.Set<T>().Add(entity);
+            dbSet.Add(entity);
         }
 
         public T Get(Expression<Func<T, bool>> filter)
         {
-            var query = _db.Set<T>().Where(filter);
+            IQueryable<T> query = dbSet;
+
+            // Include the Category navigation property if T is Product
+            if (typeof(T) == typeof(Product))
+            {
+                query = query.Include(p => ((Product)(object)p).category);
+            }
+
+            query = query.Where(filter);
             return query.FirstOrDefault();
         }
 
         public IEnumerable<T> GetAll()
         {
-            return _db.Set<T>().ToList();
+            IQueryable<T> query = dbSet;
+
+            // Include the Category navigation property if T is Product
+            if (typeof(T) == typeof(Product))
+            {
+                query = query.Include(p => ((Product)(object)p).category);
+            }
+
+            return query.ToList();
         }
 
-        public void Remove(T enttiy)
+        public void Remove(T entity)
         {
-            _db.Set<T>().Remove(enttiy);
+            dbSet.Remove(entity);
         }
 
-        public void RemoveRange(IEnumerable<T> entity)
+        public void RemoveRange(IEnumerable<T> entities)
         {
-            _db.RemoveRange(entity);
+            dbSet.RemoveRange(entities);
         }
     }
 }
